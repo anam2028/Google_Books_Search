ae7c4594b724360f0bde3ed7e68c61c900363fbd
Anamika Panna <anamika2028@gmail.com>
2020-04-08 00:46:05 -0400

    updated server.js with MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,11 +11,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
+mongoose.Promise = Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/google-books-db',
+  { useNewUrlParser: true }
+);
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
